d3de01d51d910df09397b5cbb084d3496a53bd0a
Nikita Behmetev <nikitabehmetev@gmail.com>
2022-11-09 09:36:16 +0300

    Проверка напустую корзину с помощью middleware

--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,19 @@ Route::group([
 Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
 Route::get('/categories', 'App\Http\Controllers\MainController@categories')->name('categories');
 
-Route::get('/basket', 'App\Http\Controllers\BasketController@basket')->name('basket');
-Route::get('/basket/place', 'App\Http\Controllers\BasketController@basketPlace')->name('basket-place');
-Route::post('/basket/place', 'App\Http\Controllers\BasketController@basketConfirm')->name('basket-confirm');
-Route::post('/basket/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
-Route::post('/basket/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
+Route::group(['prefix' => 'basket'], function () {
+    Route::post('/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
+
+    Route::group([
+        'middleware' => 'basket_is_not_empty',
+    ], function () {
+        Route::get('/', 'App\Http\Controllers\BasketController@basket')->name('basket');
+        Route::get('/place', 'App\Http\Controllers\BasketController@basketPlace')->name('basket-place');
+        Route::post('/place', 'App\Http\Controllers\BasketController@basketConfirm')->name('basket-confirm');
+        Route::post('/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
+    });
+});
+
 
 Route::get('/{category}', 'App\Http\Controllers\MainController@category')->name('category');
 Route::get('/{category}/{product?}', 'App\Http\Controllers\MainController@product')->name('product');
-
-Route::post('/basket/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
-Route::post('/basket/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
